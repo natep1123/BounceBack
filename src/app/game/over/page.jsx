7c8e8290612de3ab4659690fa54be5042d7f0918
Nate <nathaniel.perry1123@gmail.com>
2025-04-18 22:19:19 -0400

    styling

--- src/app/game/over/page.jsx
+++ src/app/game/over/page.jsx
@@ -16,7 +16,7 @@ export default async function GameOver() {
       <main className="flex flex-col items-center justify-center min-h-[calc(100vh-4rem)] px-4">
         <h2>Game Over!</h2>
         <Link href="/game/play">
-          <button className="mb-2 px-4 py-2 text-white bg-pink-600 border-2 border-gray-800 rounded-lg cursor-pointer">
+          <button className="mt-2 mb-4 px-4 py-2 text-white bg-pink-600 border-2 border-gray-800 rounded-lg cursor-pointer">
             Play Again?
           </button>
         </Link>
--- src/components/FinalScoreCard.jsx
+++ src/components/FinalScoreCard.jsx
@@ -2,7 +2,7 @@
 
 export default function FinalScoreCard({ finalScore }) {
   return (
-    <div className="w-full max-w-md bg-gray-800 rounded-lg shadow-lg p-6 mb-8">
+    <div className="w-full max-w-md bg-gray-800 rounded-lg shadow-lg p-6 mb-6">
       <h3 className="text-2xl font-semibold text-center text-orange-400 mb-4">
         Final Score
       </h3>
