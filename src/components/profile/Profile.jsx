f12351d47cac699e32a2622d7a8efed78c5e7396
Nate <nathaniel.perry1123@gmail.com>
2025-04-22 11:30:04 -0400

    Restructured Profile component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -25,44 +25,43 @@ export default function Profile({ username, email }) {
     fetchData();
   }, []);
 
+  if (isDeleted) {
+    return (
+      <div className="grid place-items-center h-screen bg-gray-900">
+        <div className="text-center p-6 rounded-xl bg-gray-800 border-t-4 border-red-400 shadow-xl">
+          <h2 className="text-3xl font-bold text-red-400 mb-4">
+            Account Deleted
+          </h2>
+          <p className="text-gray-200 text-lg">
+            Your account and associated data have been successfully deleted.
+          </p>
+          <p className="text-gray-400 mt-2">Redirecting to homepage...</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <>
-      {!isDeleted && (
-        <div className="min-h-screen bg-gray-900 text-white text-center">
-          <Header display="navbar" />
-          <main className="flex flex-col items-center min-h-[calc(100vh-4rem)] px-4">
-            <h2>{username}'s Profile</h2>
-            <UserInfoCard username={username} email={email} />
-            <HighScoresCard highscores={highscores} />
-            <div className="mt-4">
-              <DeleteButton
-                loading={loading}
-                setLoading={setLoading}
-                setError={setError}
-                setIsDeleted={setIsDeleted}
-              />
-              {error && (
-                <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-                  {error}
-                </div>
-              )}
+    <div className="min-h-screen bg-gray-900 text-white text-center">
+      <Header display="navbar" />
+      <main className="flex flex-col items-center min-h-[calc(100vh-4rem)] px-4">
+        <h2>{username}'s Profile</h2>
+        <UserInfoCard username={username} email={email} />
+        <HighScoresCard highscores={highscores} />
+        <div className="mt-4">
+          <DeleteButton
+            loading={loading}
+            setLoading={setLoading}
+            setError={setError}
+            setIsDeleted={setIsDeleted}
+          />
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
             </div>
-          </main>
+          )}
         </div>
-      )}
-      {isDeleted && (
-        <div className="grid place-items-center h-screen bg-gray-900">
-          <div className="text-center p-6 rounded-xl bg-gray-800 border-t-4 border-red-400 shadow-xl">
-            <h2 className="text-3xl font-bold text-red-400 mb-4">
-              Account Deleted
-            </h2>
-            <p className="text-gray-200 text-lg">
-              Your account and associated data have been successfully deleted.
-            </p>
-            <p className="text-gray-400 mt-2">Redirecting to homepage...</p>
-          </div>
-        </div>
-      )}
-    </>
+      </main>
+    </div>
   );
 }
