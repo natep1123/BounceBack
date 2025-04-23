a04aadfb31f3d1e372cd823d6abbed6e10c7482b
Nate <nathaniel.perry1123@gmail.com>
2025-04-23 16:47:55 -0400

    Styling, reduced ball animation field size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,15 @@ const Header = ({ display, setGameState, setScore }) => {
   return (
     <header className="border-b-2 border-pink-600 text-center">
       {/* Welcome Pages */}
-      {display === "welcome" && (
-        <h1 className="py-2">Welcome to BounceBack!</h1>
-      )}
+      {display === "welcome" && <h1>Welcome to BounceBack!</h1>}
 
       {/* Navbar-Included Pages */}
       {display === "navbar" && (
         <>
-          <h1 className="py-2">BounceBack!</h1>
+          <h1>BounceBack!</h1>
           <Navbar setGameState={setGameState} setScore={setScore} />
         </>
       )}
-
-      {/* Active Gameplay Page */}
-      {display === "game" && <h1 className="py-2">BounceBack!</h1>}
     </header>
   );
 };
--- src/components/game/BallAnimation.jsx
+++ src/components/game/BallAnimation.jsx
@@ -4,10 +4,10 @@ import { useEffect, useRef } from "react";
 
 // This component animates a ball bouncing around a box.
 export default function BallAnimation({ setGameState }) {
-  const ballSize = 12;
+  const ballSize = 10;
   const boxRef = useRef(null);
   const ballRef = useRef(null);
-  const velocity = useRef({ dx: 6, dy: 3 });
+  const velocity = useRef({ dx: 8, dy: 4 });
   const position = useRef({ x: 0, y: 0 });
   const animationFrameId = useRef(0);
   const boxSize = useRef({ width: 0, height: 0 });
@@ -112,7 +112,7 @@ export default function BallAnimation({ setGameState }) {
     <div className="flex flex-col items-center mt-4">
       <div
         ref={boxRef}
-        className="w-[90vw] h-[60vh] border-2 border-pink-600 bg-gray-800 relative"
+        className="w-[70vw] h-[45vh] border-2 border-pink-600 bg-gray-800 relative"
       >
         <div
           ref={ballRef}
