8e562f736c057273ee83381b3b8244b85b37090b
Nate <nathaniel.perry1123@gmail.com>
2025-04-23 08:51:36 -0400

    Added 'play to earn a score' for no-score profiles in highscores card

--- src/components/HighScoresCard.jsx
+++ src/components/HighScoresCard.jsx
@@ -1,11 +1,22 @@
 "use client";
+import { use, useEffect, useState } from "react";
 
 export default function HighScoresCard({ highscores }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (highscores?.scores.length !== 0) {
+      setLoading(false);
+    }
+  }, [highscores]);
+
   return (
     <div className="w-full max-w-md bg-gray-800 rounded-lg shadow-lg p-6 mb-4">
       <h3 className="text-2xl font-semibold text-center text-orange-400 mb-4">
         High Scores
       </h3>
+
+      {/* Highscores list */}
       {highscores?.scores && highscores.scores.length > 0 ? (
         <div className="space-y-4">
           {highscores.scores.map((score, index) => (
@@ -26,8 +37,15 @@ export default function HighScoresCard({ highscores }) {
           ))}
         </div>
       ) : (
-        <p className="text-center text-gray-400">Loading...</p>
+        <p className="text-center text-gray-400">
+          {loading ? "Loading..." : null}
+          {highscores?.scores.length === 0
+            ? "Play a game to earn a score!"
+            : null}
+        </p>
       )}
+
+      {/* Message (e.g. top scores retrieved) */}
       {highscores?.message && (
         <p className="text-center text-gray-400 mt-4">{highscores.message}</p>
       )}
