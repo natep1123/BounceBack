5c602eef9d651007ca2f9c0c10dec1c53c1a7a92
Nate <nathaniel.perry1123@gmail.com>
2025-05-07 09:01:05 -0400

    Styling

--- src/components/game/PlayScreen.jsx
+++ src/components/game/PlayScreen.jsx
@@ -2,7 +2,7 @@ import ClientGame from "./ClientGame";
 
 export default function PlayScreen() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-900 ">
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
       <ClientGame />
     </main>
   );
